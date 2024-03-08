494546993dcce507de6f6fc96d92db00ed6ac1d0
Asim Mutlu <asimmutlu6134@gmail.com>
2024-03-08 18:12:48 +0300

    Comment by id added and more.

---
+++ TraversalCoreProje/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new(new EfCommentDal());
+        [HttpGet]
+        public PartialViewResult AddComment(int id)
+        {
+            ViewBag.Id = id;
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult AddComment(Comment c)
+        {
+            c.Date = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            c.State = true;
+            commentManager.TAdd(c);
+            return RedirectToAction("Index","Destination");
+        }
+    }
+}
