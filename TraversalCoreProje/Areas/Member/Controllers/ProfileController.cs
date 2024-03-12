f63f3e9246137be0669ba4f239d06472a8ae10dd
Asim Mutlu <asimmutlu6134@gmail.com>
2024-03-12 17:46:53 +0300

    Get login information and more.

--- TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ProfileController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProje.Areas.Member.Models;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
 {
+    [Area("Member")]
+    [Route("Member/[Controller]/[action]")]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+        public ProfileController(UserManager<AppUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel userEditViewModel = new UserEditViewModel();
+            userEditViewModel.name = values.Name;
+            userEditViewModel.surName = values.Surname;
+            userEditViewModel.phoneNumber = values.PhoneNumber;
+            userEditViewModel.mail = values.Email;
+            return View(userEditViewModel);
         }
     }
 }
--- TraversalCoreProje/Controllers/LoginController.cs
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -66,7 +66,7 @@ namespace TraversalCoreProje.Controllers
                 var result = await _signInManager.PasswordSignInAsync(u.username, u.password, false, true);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Destination");
+                    return RedirectToAction("Index", "Profile", new { area = "Member" });
                 }
                 else
                 {
