a34e24dc91277c3b2defa7be149c7fa53592962e
Asim Mutlu <asimmutlu6134@gmail.com>
2024-03-13 13:42:55 +0300

    update process added in profile setting.

--- DataAccessLayer/Migrations/20240311142951_mig_add_identity.cs
+++ DataAccessLayer/Migrations/20240311142951_mig_add_identity.cs
@@ -30,10 +30,10 @@ namespace DataAccessLayer.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Name = table.Column<string>(type: "nvarchar(256)", nullable: false),
                     Surname = table.Column<string>(type: "nvarchar(256)", nullable: false),
-                    Gender = table.Column<string>(type: "nvarchar(256)", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(256)", nullable: true),
                     UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
