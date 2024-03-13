0ab494d2d5233238ca12ae6b8e54ed01990b8dd0
Asim Mutlu <asimmutlu6134@gmail.com>
2024-03-13 15:55:29 +0300

    Reservation Page edited and more.

--- TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -12,13 +13,27 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         DestinationManager destinationManager = new(new EfDestinationDal());
         ReservationManager reservationManager = new(new EfReservationDal());
 
+        private readonly UserManager<AppUser> _userManager;
+
+        public ReservationController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult MyCurrentReservation()
         {
+
             return View();
         }
         public IActionResult MyOldReservation()
         {
             return View();
+        }
+        public async Task<IActionResult> MyApprovalReservation()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            var valueList = reservationManager.GetListApprovalReservation(values.Id);
+            return View(valueList);
         }
         [HttpGet]
         public IActionResult NewReservation()
@@ -36,7 +51,7 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         public IActionResult NewReservation(Reservation r)
         {
             r.AppUserId = 1;
-            r.Status = "false";
+            r.Status = "Onay Bekleniyor";
             reservationManager.TAdd(r);
             return RedirectToAction("MyCurrentReservation");
         }
