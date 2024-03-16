08a045fa9410c0a2b8639bf7ef892bd261d9c154
Asim Mutlu <asimmutlu6134@gmail.com>
2024-03-16 14:12:01 +0300

    ProfileInformation Component edited.

---
+++ TraversalCoreProje/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation :ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task <IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.memberName = values.Name + " " + values.Surname;
+            ViewBag.memberPhone = values.PhoneNumber;
+            ViewBag.memberMail = values.Email;
+            return View();
+        }
+    }
+}
