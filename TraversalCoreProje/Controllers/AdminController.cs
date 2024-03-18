13eeca1b73fd60b90d773b70d287a2d4a9c68a44
Asim Mutlu <asimmutlu6134@gmail.com>
2024-03-18 14:15:02 +0300

    Admin Page added and more.

---
+++ TraversalCoreProje/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialAppBrandDemo()
+        {
+            return PartialView();
+        }
+    }
+}
--- TraversalCoreProje/Program.cs
+++ TraversalCoreProje/Program.cs
@@ -60,7 +60,13 @@ app.UseEndpoints(endpoints =>
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
     );
 });
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 app.Run();
 
 
