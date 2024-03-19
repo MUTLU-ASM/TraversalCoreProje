e61495363db5a55d254857be48198b95c3fcb784
Asim Mutlu <asimmutlu6134@gmail.com>
2024-03-19 16:05:30 +0300

    Admin Dashboard added and more.

---
+++ TraversalCoreProje/ViewComponents/AdminDashboard/_Cards1Statistic.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.AdminDashboard
+{
+    public class _Cards1Statistic:ViewComponent
+    {
+        Context db=new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = db.Destinations.Count();
+            ViewBag.v2 = db.Users.Count();
+            return View();
+        }
+    }
+}
